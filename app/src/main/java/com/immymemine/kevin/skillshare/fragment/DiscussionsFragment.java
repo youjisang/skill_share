4bf69d60c4ff6e0b4f8545aae7f5432c085a7e86
youjisang <jisangman89@gmail.com>
2017-12-05 14:48:07 +0900

    12/06

--- app/src/main/java/com/immymemine/kevin/skillshare/fragment/DiscussionsFragment.java
+++ app/src/main/java/com/immymemine/kevin/skillshare/fragment/DiscussionsFragment.java
@@ -12,6 +12,7 @@ import android.view.ViewGroup;
 import android.widget.FrameLayout;
 
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.immymemine.kevin.skillshare.R;
@@ -36,9 +37,9 @@ public class DiscussionsFragment extends Fragment {
 
     private RecyclerView discussionsRecyclerView;
     private View view;
-//    private DiscussionsAdapter adapter;
+    //    private DiscussionsAdapter adapter;
     private List<DiscussionModel> discussionData = new ArrayList<>();
-    private FrameLayout layout_frame_discussion;
+    private LinearLayout layout_frame_discussion;
 
     TextView textViewDiscussion;
     RecyclerView recyclerViewDiscussion;
@@ -60,7 +61,7 @@ public class DiscussionsFragment extends Fragment {
 
         initView();
 //        initRecycler();
-        onScrollReActing();
+//        onScrollReActing();
 
         textViewDiscussion = view.findViewById(R.id.text_view_discussion);
 
@@ -79,12 +80,13 @@ public class DiscussionsFragment extends Fragment {
 
     private void initView() {
         discussionsRecyclerView = view.findViewById(R.id.recycler_view_discussion);
-//        layout_frame_discussion = view.findViewById(R.id.layout_frame_discussion);
+        layout_frame_discussion = view.findViewById(R.id.layout_frame_discussion);
+    }
 
     public void addDiscussion(View view) {
         EditText editText = view.findViewById(R.id.editText);
         String input = editText.getText().toString();
-        if( !ValidationUtil.isEmpty(input) ) {
+        if (!ValidationUtil.isEmpty(input)) {
             editText.setText("");
             // discussion 세팅
             Discussion discussion = new Discussion();
@@ -100,7 +102,7 @@ public class DiscussionsFragment extends Fragment {
 
     private void handleResponse(List<Discussion> discussions) {
         // list reverse...
-        if(adapter == null) {
+        if (adapter == null) {
             adapter = new DiscussionsAdapter(discussions);
             recyclerViewDiscussion.setAdapter(adapter);
         } else {
@@ -119,23 +121,23 @@ public class DiscussionsFragment extends Fragment {
     // 리싸이클러 뷰가 움직일 때, 프레임 레이아웃이 사라지게 하는 메서드
     // coodinatorLayout으로 하는 방법이 머티리얼 디자인 가이드에서 더 추천??하는 것 같음.
     // 해당 프래그먼트의 밑으로 내리는 이벤트가 생길떄 클래스액티비티와 통신을 해서, 탭레이아웃을 사라지게 해야하는 것인지 고민이 필요함.
-    private void onScrollReActing() {
-        discussionsRecyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
-            @Override
-            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
-                if (dy > 0 && layout_frame_discussion.isShown()) {
-                    layout_frame_discussion.setVisibility(View.GONE);
-                } else if (dy < 0 ) {
-                    layout_frame_discussion.setVisibility(View.VISIBLE);
-                }
-            }
-
-            @Override
-            public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
-                super.onScrollStateChanged(recyclerView, newState);
-            }
-        });
-    }
+//    private void onScrollReActing() {
+//        discussionsRecyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+//            @Override
+//            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+//                if (dy > 0 && layout_frame_discussion.isShown()) {
+//                    layout_frame_discussion.setVisibility(View.GONE);
+//                } else if (dy < 0) {
+//                    layout_frame_discussion.setVisibility(View.VISIBLE);
+//                }
+//            }
+//
+//            @Override
+//            public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
+//                super.onScrollStateChanged(recyclerView, newState);
+//            }
+//        });
+//    }
 
 
 }
--- app/src/main/java/com/immymemine/kevin/skillshare/utility/PreferenceUtill.java
+++ app/src/main/java/com/immymemine/kevin/skillshare/utility/PreferenceUtill.java
@@ -3,25 +3,33 @@ package com.immymemine.kevin.skillshare.utility;
 import android.content.Context;
 import android.content.SharedPreferences;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Created by JisangYou on 2017-12-02.
  */
 
 public class PreferenceUtill {
 
+    private static final String filename = "skillshareSharedPreference";
 
-//    private static SharedPreferences getPreference(Context context){
-//        return context.getSharedPreferences(/**/Context.MODE_PRIVATE);
-//    }
-//
-//    public static void setValue(Context context, String key, String value){
-//        SharedPreferences.Editor editor = getPreference(context).edit();
-//        editor.putString(key, value);
+    private static SharedPreferences getPreference(Context context){
+        return context.getSharedPreferences(filename,Context.MODE_PRIVATE);
+    }
+
+    public static void setValue(Context context, String key, ArrayList<String> value){
+        SharedPreferences.Editor editor = getPreference(context).edit();
+        Set<String> set = new HashSet<String>();
+        set.addAll(value);
+        editor.putStringSet(key, set);
+        editor.apply();
 //        editor.commit();
-//    }
-//
-//    public static String getString(Context context, String key){
-//        return getPreference(context).getString(key,"");
-//    }
+    }
+
+    public static ArrayList<String> getString(Context context, String key){
+        return (ArrayList<String>) getPreference(context).getStringSet("",null);
+    }
 
 }
